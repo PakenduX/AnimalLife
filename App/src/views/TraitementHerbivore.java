64ace827ac49e44f96ea7f1f33b685d9267225ca
Mama <mamadembele7@gmail.com>
2019-01-04 23:54:15 +0100

    maj

--- App/src/views/TraitementHerbivore.java
+++ App/src/views/TraitementHerbivore.java
@@ -20,13 +20,23 @@ public class TraitementHerbivore implements Runnable {
 
 	public void run() {
 		int i = 2;
+		int j = 6;
 		while(true) {
-			if (i == 3)
-				i = 1;
-			else
-				i++;
+			if(a.getDeplacementVersGauche()) {
+				if (i == 3)
+					i = 1;
+				else
+					i++;
 
-			animal.setIcon(new ImageIcon("/home/pkss/AnimalLife/App/images/pas_" + i + ".png"));
+				animal.setIcon(new ImageIcon("/home/pkss/AnimalLife/App/images/pas_" + i + ".png"));
+			}else{
+				if (j == 8)
+					j = 5;
+				else
+					j++;
+
+				animal.setIcon(new ImageIcon("/home/pkss/AnimalLife/App/images/pas_" + j + ".png"));
+			}
 			a.seDeplacer(panneau.getWidth(), panneau.getHeight());
 			Jeu.traiter_collisions();
 			animal.setLocation(a.getPosition().getX(), a.getPosition().getY());
